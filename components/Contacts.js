a04c35e971f05267f59e3744037a8078ffd63b1e
wojjar <jaroslaw@wojcik.hub.pl>
2018-01-29 00:39:42 +0100

    Add files via upload

---
+++ components/Contacts.js
@@ -0,0 +1,17 @@
+var Contacts = React.createClass({
+  propTypes: {
+    items: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+    var contacts = this.props.items.map(function(contact) {
+        return <Contact item={contact} key={contact.id} />
+    });
+
+    return (
+      <div>
+          <ul className={'contactslist'}> {contacts}  </ul>
+      </div>
+    );
+  }
+});
